af3ade03fa5043dd20afb4085f9a3c863e79978f
Yubeen Kim <cz9812@naver.com>
2022-11-22 17:40:57 +0900

    hi

---
+++ HW/myProject/3.c
@@ -0,0 +1,7 @@
+#include "myProject.h"
+
+int printMyName3(void)
+{
+	printf("3.my name is Sangjin Lee\n");
+	return 1;
+}
---
+++ HW/myProject/myProject.h
@@ -0,0 +1,7 @@
+#ifndef _myprogect_h_
+#define _myproject_h_
+int printMyName1(void);
+int printMyName2(void);
+int printMyName3(void);
+
+#endif
