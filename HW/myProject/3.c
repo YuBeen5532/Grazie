dc5a4a981a71914ee77555cec792b668c91dfe35
ppdl777 <ppdl777@naver.com>
2022-11-25 20:48:50 +0900

    fndtest완성

--- HW/myProject/3.c
+++
@@ -1,7 +0,0 @@
-#include "myProject.h"
-
-int printMyName3(void)
-{
-	printf("3.my name is Sangjin Lee\n");
-	return 1;
-}
--- HW/myProject/main.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-#include "myProject.h"
-int main(void)
-{
-	printf ("Chief name is Yubeen Kim\n");
-	printMyName1();
-	printMyName2();
-    printMyName3();
-}
