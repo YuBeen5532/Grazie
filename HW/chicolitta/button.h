6c9b00bf9a43de75b8671ec9553c31c57803f050
ohseungyeonnn <djaaj0917@mju.ac.kr>
2022-11-25 14:12:49 +0900

    button.h

--- HW/chicolitta/button.h
+++ HW/chicolitta/button.h
@@ -1,12 +1,17 @@
 #ifndef _BUTTON_H_
 #define _BUTTON_H_
+
 #define MESSAGE_ID 1122
+
 typedef struct
 {
 long int messageNum;
 int keyInput;
 int pressed;
 } BUTTON_MSG_T;
+
 int buttonInit(void);
+//void buttonThFunc(void); 이건 필요없나????
 int buttonExit(void);
+
 #endif
