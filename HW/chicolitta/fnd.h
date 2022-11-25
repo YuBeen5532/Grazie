a988c6eaa10a65f779f1c4b89332a7f619fcf011
ppdl777 <ppdl777@naver.com>
2022-11-25 20:54:16 +0900

    fndtest

---
+++ HW/chicolitta/fnd.h
@@ -0,0 +1,1 @@
+int fndDisp(void); //int fndDips 함수 원형 (이후 main에서 불러씀)
---
+++ HW/chicolitta/fndtest.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/ioctl.h>
+#include <ctype.h>
+#include <sys/ipc.h>
+#include <sys/types.h>
+#include <fcntl.h>
+#include <unistd.h>
+
+#define FND_DRIVER_NAME "/dev/perifnd"
+
+int main(void)
+{
+fndDisp(990819,10);
+
+return 0;
+}
+
