e83664870eaa78b21930f2665959df29fb7627bf
zolida <kavin0530@naver.com>
2022-11-26 00:49:52 +0900

    buzzer good

--- HW/chicolitta/buzzertest.c
+++ HW/chicolitta/buzzertest.c
@@ -1,56 +1,45 @@
 #include <stdio.h>
-#include <stdio.h>
-#include <sys/ioctl.h>
-#include <ctype.h>
-#include <sys/ipc.h>
-#include <sys/types.h>
-#include <sys/types.h>
-#include <sys/stat.h>
-
 #include <stdlib.h>
 #include <string.h>
-#include <fcntl.h>
 #include <unistd.h>
-#include <dirent.h>
+#define MAX_SCALE_STEP 8
 
+char gBuzzerBaseSysDir[128]; ///sys/busz/platform/devices/peribuzzer.XX 가 결정됨
 const int musicScale[MAX_SCALE_STEP] =
 {
-262, /*do*/ 294,330,349,392,440,494, /* si */ 523
+    262, /*do*/ 294,330,349,392,440,494, /* si */ 523
 };
+
 
 
 int main(int argc , char **argv)
 {
     int freIndex;
     if (argc < 2 || findBuzzerSysPath() )
-        {
+    {
         printf("Error!\n");
         doHelp();
         return 1;
-        }
-
-
+    }
 
     freIndex = atoi(argv[1]);
     printf("freIndex :%d \n",freIndex);
 
     if ( freIndex > MAX_SCALE_STEP )
-        {
+    {
         printf(" <buzzerNo> over range \n");
         doHelp();
         return 1;
-        }
+    }
 
     if ( freIndex == 0)// disable
-        {
-        buzzerEnable(0);
-        }
+    {
+        buzzerStopSong(0);
+    }
     else
-        {
-        setFrequency(musicScale[freIndex-1]);
-        buzzerEnable(1);
-        }
-return 0;
+    {
+        buzzerPlaySong(musicScale[freIndex-1]);
+    }
+    
+    return 0;
 }
-
-
