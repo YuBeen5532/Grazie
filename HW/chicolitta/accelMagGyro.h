251be894b6baeeea334dce68af2afb4864da1bce
zolida <kavin0530@naver.com>
2022-12-20 15:40:10 +0900

    accel

--- HW/chicolitta/accelMagGyro.h
+++ HW/chicolitta/accelMagGyro.h
@@ -1,12 +1,7 @@
 #ifndef _ACCELMAG_H_
 #define _ACCELMAG_H_
 #define ACCELPATH "/sys/class/misc/FreescaleAccelerometer/"
-#define MAGNEPATH "/sys/class/misc/FreescaleMagnetometer/"
-#define GYROPATH "/sys/class/misc/FreescaleGyroscope/"
-int accInit(void);
-int magInit(void);
-int gyroInit(void);
-int* getAcc(void);
-int getMag(void);
-int getGyro(void);
+void accInit(void);
+int getAcc(void);
 #endif
+
--- HW/chicolitta/accelMagGyrotest.c
+++ HW/chicolitta/accelMagGyrotest.c
@@ -2,19 +2,23 @@
 #include <stdlib.h>
 #include <fcntl.h>
 #include <unistd.h>
-
 #include "accelMagGyro.h"
+#include "buzzer.h"
+#include "button.h"
 
-int main()
-{
-    accInit();
-    getAcc();
-    
-    magInit();
-    getMag();
 
-	gyroInit();
-	getGyro();
-    
-    return 0;
+int main(){
+findBuzzerSysPath();
+buttonInit();
+accInit();
+for(int i = 0; i<3; i++){
+int Value = getAcc();
+printf("%d\r\n", Value);
+sleep(1);
+}
+int value = button_rcv();
+printf("%d\r\n",value);
+buzzerPlaySong(musicScale[4]);
+usleep(500000);
+buzzerStopSong();
 }
