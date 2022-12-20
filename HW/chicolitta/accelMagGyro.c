965d202ee8d4f02ec1b4b6f00d844eb0e44e208b
zolida <kavin0530@naver.com>
2022-12-20 15:48:02 +0900

    accelMagGyro.c

--- HW/chicolitta/accelMagGyro.c
+++ HW/chicolitta/accelMagGyro.c
@@ -5,72 +5,24 @@
 #include "accelMagGyro.h"
 
 
-int fd = 0;
-FILE *fp = NULL;
+int fd_acc = 0;
+FILE *fp_acc;
 
-int accInit(void){
+void accInit(void){
 
-	fd = open (ACCELPATH "enable",O_WRONLY);
-	dprintf (fd,"1");
-	close(fd);
-	
-	return 0;
+   fd_acc = open (ACCELPATH "enable",O_WRONLY);
+   dprintf (fd_acc,"1");
+   close(fd_acc);
 }
-	
-int magInit(void ){
 
-	fd = open (MAGNEPATH "enable",O_WRONLY);
-	dprintf (fd,"1");
-	close(fd);
-	
-	return 0;
-}
-	
-int gyroInit(void){
 
-	fd = open (GYROPATH "enable",O_WRONLY);
-	dprintf (fd,"1");
-	close(fd);
-	
-	return 0;
-}
-	
-int* getAcc(void){
+int getAcc(void){
+   fp_acc = fopen (ACCELPATH "data", "rt");
+   int accel[3];
+   fscanf(fp_acc,"%d, %d, %d",&accel[0],&accel[1],&accel[2]);
+   
+   fclose(fp_acc);
+   
+   return accel[0];
 
-	fp = fopen (ACCELPATH "data", "rt");
-
-	int accel[3];
-
-	fscanf(fp,"%d, %d, %d",&accel[0],&accel[1],&accel[2]);
-	printf ("I read Accel %d, %d, %d\r\n",accel[0],accel[1],accel[2]);
-	fclose(fp);
-	
-	return accel;
-
-}
-	
-int getMag(void){
-
-	fp = fopen (MAGNEPATH "data", "rt");
-
-	int magne[3];
-
-	fscanf(fp,"%d, %d, %d",&magne[0],&magne[1],&magne[2]);
-	printf ("I read Magneto %d, %d, %d\r\n",magne[0],magne[1],magne[2]);
-	fclose(fp);
-	
-	return 0;
-}
-	
-int getGyro(void){
-
-	fp = fopen (GYROPATH "data", "rt");
-
-	int gyro[3];
-
-	fscanf(fp,"%d, %d, %d",&gyro[0],&gyro[1],&gyro[2]);
-	printf ("I read Gyroscope %d, %d, %d\r\n",gyro[0],gyro[1],gyro[2]);
-	fclose(fp);
-	
-	return 0;
 }
