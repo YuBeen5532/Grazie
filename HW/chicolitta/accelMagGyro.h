57ca623932598751f30bcd78c0ec63e17c0905c4
ohseungyeonnn <djaaj0917@mju.ac.kr>
2022-12-21 14:55:56 +0900

    accel.h 수정

--- HW/chicolitta/accelMagGyro.h
+++ HW/chicolitta/accelMagGyro.h
@@ -1,7 +1,9 @@
 #ifndef _ACCELMAG_H_
 #define _ACCELMAG_H_
-#define ACCELPATH "/sys/class/misc/FreescaleAccelerometer/" //application 상에서 DD 억세스 위한 경로
+#define ACCELPATH "/sys/class/misc/FreescaleAccelerometer/" //application에서 access하기 위한 경로
+
 void accInit(void);
 int getAcc(void);
+
 #endif
 
