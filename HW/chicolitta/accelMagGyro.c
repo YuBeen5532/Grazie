626b40bf322673584646dae5f9d8a758ff4f29c0
ohseungyeonnn <djaaj0917@mju.ac.kr>
2022-12-21 14:52:55 +0900

    accel 주석 수정

--- HW/chicolitta/accelMagGyro.c
+++ HW/chicolitta/accelMagGyro.c
@@ -4,25 +4,26 @@
 #include <unistd.h>
 #include "accelMagGyro.h"
 
-
 int fd_acc = 0;
 FILE *fp_acc;
 
-void accInit(void){ // 가속도계 초기 설정
+/*============================== Accel INIT ==============================*/
+void accInit(void){                              // 가속도계 초기 설정
 
-   fd_acc = open (ACCELPATH "enable",O_WRONLY); // enable 파일 오픈
-   dprintf (fd_acc,"1"); // 1 써주기
-   close(fd_acc); // 파일 닫기
+   fd_acc = open (ACCELPATH "enable",O_WRONLY);  // enable file open
+   dprintf (fd_acc,"1");                         // enable file에 1 써준 후
+   close(fd_acc);                                // file close
 }
+/*===================================================================================*/
 
-
+/*============================== Accel 값 읽기 =================================*/
 int getAcc(void){
-   fp_acc = fopen (ACCELPATH "data", "rt"); // data 값 읽기 위해 파일 오픈
-   int accel[3];
-   fscanf(fp_acc,"%d, %d, %d",&accel[0],&accel[1],&accel[2]); // x축, y축, z축
-   
-   fclose(fp_acc); // 파일 클로즈
-   
-   return accel[0]; // x축 가속도 리턴
 
+   fp_acc = fopen (ACCELPATH "data", "rt");      // data 값 읽기 위해 file open
+   int accel[3];
+   fscanf(fp_acc,"%d, %d, %d",&accel[0],&accel[1],&accel[2]); // x축, y축, z축   
+   fclose(fp_acc);                                            // file close
+   
+   return accel[0];                           // accel[0]값 => x축 가속도 return
 }
+/*===================================================================================*/
